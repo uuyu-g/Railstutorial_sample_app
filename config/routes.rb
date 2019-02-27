fb4091c5ecaa29efd03ddead061b5432e758705b
yutaro <aqueous.mkli@gmail.com>
2019-02-27 15:42:54 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get '/help', to: 'static_pages#help'
-  get '/about', to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
-  get '/signup', to: 'users#new'
+  get '/help' => 'static_pages#help'
+  get '/about' => 'static_pages#about'
+  get '/contact' => 'static_pages#contact'
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create' 
   resources :users
 end
