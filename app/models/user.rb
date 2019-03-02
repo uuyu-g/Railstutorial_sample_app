14f3d563adf825d3ab8991ded8b8252cd6444ea0
yutaro <aqueous.mkli@gmail.com>
2019-03-02 20:02:20 +0900

    Implement basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,16 @@ class User < ApplicationRecord
 										uniqueness: {case_sensitive: false}
 	has_secure_password
 	validates :password, presence: true, length: {minimum: 6}
+
+	def User.digest(string)
+		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+																									BCrypt::Engine.cost
+		BCrypt::Password.create(string, cost: cost)
+	end
+
+	# def User.digest(string)
+  #   cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+  #                                                 BCrypt::Engine.cost
+  #   BCrypt::Password.create(string, cost: cost)
+  # end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  test "inviled signup information" do 
+  test "無効な情報でのサインアップをはじく" do 
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, params: {
@@ -13,5 +13,18 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
         }}
     end
     assert_template 'users/new'
+  end
+
+  test "有効な情報でサインアップ" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert is_logged_in?
   end
 end
