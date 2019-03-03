a851bdac0a40883df9f347f5f7becce0a62a6066
yutaro <aqueous.mkli@gmail.com>
2019-03-03 09:32:38 +0900

    テストにコメントを追加

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -17,22 +17,27 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   end
 
   test "有効な情報でログインし、後にログアウト" do
+    ##ログインの確認
+    # ログイン用のパスを開く
     get login_path
-    post login_path,params:{session: {email: @user.email, password: 'password'}}
+    # セッション用パスに有効な情報をPOSTする
+    post login_path, params:{session: {email: @user.email, password: 'password'}}
     assert is_logged_in?
     assert_redirected_to @user #リダイレクト先が正しいか
     follow_redirect! #実際にリダイレクト先に移動
     assert_template 'users/show'
-    # ハイパーリンクのテスト
+    # ログイン用のリンクが表示されなくなったことを確認する
     assert_select "a[href=?]", login_path, count:0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
-    # ログアウトのテスト
+    
+    ## ログアウトのテスト
+    # ログアウト用パスにDELETEする
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
     follow_redirect!
-    # ハイパーリンクのテスト
+    # ログアウト用のリンクが表示されなくなったのを確認
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count:0
     assert_select "a[href=?]", user_path(@user),count:0
